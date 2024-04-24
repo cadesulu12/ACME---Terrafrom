544385cbbc5e3852c6b583107fbbc1bf16a41508
cadesulu12 <168036373+cadesulu12@users.noreply.github.com>
2024-04-24 18:57:41 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"  # Update with your desired region
+  region = "us-east-1" 
 }
 
 module "vpc" {
@@ -10,5 +10,5 @@ module "ec2" {
   source        = "./modules/ec2"
   vpc_id        = module.vpc.vpc_id
   subnet_ids    = module.vpc.private_subnet_ids
-  instance_count = 2  # Adjust the number of instances as needed
+  instance_count = 2  
 }
