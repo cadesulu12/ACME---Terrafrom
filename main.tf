d2b39d4d101b2b9b255b3f691db8d8ac6830a029
cadesulu12 <168036373+cadesulu12@users.noreply.github.com>
2024-04-28 18:33:07 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  count         = 2
   ami           = "ami-09b90e09742640522"
   instance_type = "t2.micro"
-  vpc_id        = module.vpc.vpc_id
-  subnet_ids    = module.vpc.private_subnet_ids
 }
